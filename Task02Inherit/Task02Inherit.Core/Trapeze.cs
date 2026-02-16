dfea13262ed18d376f62792529feb97427dfe078
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2026-02-16 16:25:58 -0500

    Task 02 Inheritance Completed.

---
+++ Task02Inherit/Task02Inherit.Core/Trapeze.cs
@@ -0,0 +1,37 @@
+﻿using System.Xml.Linq;
+
+namespace Task02Inherit.Core;
+
+public class Trapeze : Triangle
+{
+    private double _d;
+
+    public Trapeze(string nameof, double a, double b, double c, double d, double h) : base(nameof, a, b, c, h)
+    {
+        Name = nameof;
+        A = a;
+        B = b;
+        C = c;
+        D = d;
+        H = h;
+    }
+
+    public double D
+    {
+        get => _d;
+        set => _d = ValidateD(value);
+    }
+
+    public override double GetArea() => (B + D) * H / 2;
+
+    public override double GetPerimeter() => A + B + C + D;
+
+    private double ValidateD(double d)
+    {
+        if (d < 0)
+        {
+            throw new Exception($"The side: {d}, is not valid");
+        }
+        return d;
+    }
+}
